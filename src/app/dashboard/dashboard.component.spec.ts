488183fb92f5d52ebcb8e52a6d00413e6e4651c3
Elise St Hilaire <eliseylenore@gmail.com>
2017-10-09 15:41:18 -0700

    Create basic tests with Karma

---
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -0,0 +1,36 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { DashboardComponent } from './dashboard.component';
+
+describe('DashboardComponent', () => {
+  let comp: DashboardComponent;
+  let fixture: ComponentFixture<DashboardComponent>;
+  let de: DebugElement;
+  let el: HTMLElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ DashboardComponent ] // declare the test component
+    });
+
+    fixture = TestBed.createComponent(DashboardComponent);
+    comp = fixture.componentInstance;  // DashboardComponent test instance
+
+    //query for the title <h1> by CSS element selector
+    de = fixture.debugElement.query(By.css('h1'));
+    el = de.nativeElement;
+  });
+
+  it('should display original title', () => {
+    fixture.detectChanges();
+    expect(el.textContent).toContain(comp.title);
+  });
+
+  it('should display a different test title', () => {
+    comp.title = 'Test Title';
+    fixture.detectChanges();
+    expect(el.textContent).toContain('Test Title');
+  });
+});
