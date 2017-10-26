e29dcfc2a3d8ab9b4c3b4306f91ac3694b1ffdba
Elise St Hilaire <eliseylenore@gmail.com>
2017-10-26 12:36:24 -0700

    Create form

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';4
 import { AngularFireDatabase } from 'angularfire2/database';
-import { Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { Validators, FormControl, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
@@ -9,9 +9,24 @@ import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 })
 export class ContactFormComponent implements OnInit {
 
-  constructor() { }
+  messageForm: FormGroup;
+  order: any;
+
+  constructor(
+    private db: AngularFireDatabase,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.buildForm();
+    this.order = this.db.object('/messages/testMessage123')
+  }
+
+  buildForm() {
+    this.messageForm = this.fb.group({
+      name: new FormControl(),
+      comment: new FormControl()
+    })
   }
 
 }
