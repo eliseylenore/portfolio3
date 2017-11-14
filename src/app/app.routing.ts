7b68a26f853a428e73cf7d667a21b6e40e358688
Elise St Hilaire <eliseylenore@gmail.com>
2017-11-14 10:58:52 -0800

    merge about and home

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -9,7 +9,7 @@ import { ProjectDetailComponent } from './project-detail/project-detail.componen
 const appRoutes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    component: AboutComponent
   },
   {
     path: 'projects',
